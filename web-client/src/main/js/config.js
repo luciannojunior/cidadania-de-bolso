b983e78f64929864632ce43b1f0360337d8cfd14
Eric Breno <eric.santos@ccc.ufcg.edu.br>
2017-06-10 14:23:55 -0300

    base

---
+++ web-client/src/main/js/config.js
@@ -0,0 +1,19 @@
+(() => {
+    const app = angular.module('cidadaniaApp', ['ngMaterial', 'ui.router']);
+
+    app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function ($stateProvider, $locationProvider, $urlRouterProvider) {
+        // $locationProvider.html5Mode({
+        //     enabled: true,
+        //     requireBase: false
+        // });
+
+        $urlRouterProvider.otherwise('/home');
+
+        // $stateProvider
+        //     .state('home', {
+        //         url: '/home',
+        //         template: '<home-page><\home-page>'
+        //     })
+        //});;
+    }]);
+})();
